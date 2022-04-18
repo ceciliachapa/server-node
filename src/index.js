4e8894e64b4b62e42921de079cb4ffbd68305681
ceciliachapa <cecilia.chapa91@gmail.com>
2022-04-17 20:10:52 -0500

    Added Cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
-
-
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
 import config from './utils/config'
 import logger from './utils/logger'
 
@@ -9,6 +9,12 @@ const app = express()
 
 
 app.use(logger.middleware)
+app.use(helmet())
+app.use(
+  cors({
+    origin: config.origin,
+  }),
+)
 
 app.get('/', (req, res) => {
       logger.log.success('Calling Cecilias Root')
